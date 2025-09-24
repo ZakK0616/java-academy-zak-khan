04015f44c218134820ffb691eb658939a78a3aa9
Zak Khan <sonnymidget@gmail.com>
2025-09-24 13:48:23 -0700

    Did the Reciept.java

---
+++ workbook1d/Reciept.java/src/main/java/com/pluralsight/Main.java
@@ -0,0 +1,13 @@
+package com.pluralsight;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        int howMany = 1;
+        double price = 59.99;
+
+        System.out.println("You bought " + howMany + " game for $" + price);
+
+    }
+}
