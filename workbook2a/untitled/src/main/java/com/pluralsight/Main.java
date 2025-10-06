442561d64d9ab6f4e6451dc3a0c5db7dbe78f42b
Zak Khan <sonnymidget@gmail.com>
2025-10-06 10:17:16 -0700

    Made it loop to where java talks five times

---
+++ workbook2a/untitled/src/main/java/com/pluralsight/Main.java
@@ -0,0 +1,25 @@
+package com.pluralsight;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        int x = 10;
+
+        if (x < 5 ) {
+
+            System.out.println("Low");
+
+        } else if (x < 15) {
+
+            System.out.println("Medium");
+
+        } else {
+
+            System.out.println("High");
+
+        }
+
+
+    }
+}
