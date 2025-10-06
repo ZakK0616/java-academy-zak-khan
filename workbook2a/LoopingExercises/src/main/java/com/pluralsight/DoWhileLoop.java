dbda2db5bceadba08c25926c81565ae9f8e99908
Zak Khan <sonnymidget@gmail.com>
2025-10-06 13:40:18 -0700

    Made it loop to where java talks five times made a timer that says launch with loop

---
+++ workbook2a/LoopingExercises/src/main/java/com/pluralsight/DoWhileLoop.java
@@ -0,0 +1,15 @@
+package com.pluralsight;
+
+public class DoWhileLoop {
+        public static void main(String[] args) {
+
+            int count = 1;
+            while (count <= 5) {
+                System.out.println(" I love java! ");
+                count++;
+
+
+            }
+        }
+
+    }
